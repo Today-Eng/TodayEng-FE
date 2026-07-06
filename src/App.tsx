e6496a39518c0f5e37fe419653fdf25e16532830
kimseongyeon <seongyeon0810@naver.com>
2026-07-06 13:44:49 +0900

    chore: React + TypeScript + Tailwind CSS + PWA 프로젝트 초기 세팅

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-50">
+      <h1 className="text-3xl text-title1 font-bold text-main-500">TodayEng</h1>
+    </div>
+  )
+}
+
+export default App
