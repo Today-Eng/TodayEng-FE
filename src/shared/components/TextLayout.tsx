9094b1eb877a57509e2b5463e3e1d3e9c0ceeee3
kimseongyeon <seongyeon0810@naver.com>
2026-07-25 03:17:00 +0900

    refactor: TextLayout 컴포너트에서 mainTextSize, center prop 추가

--- src/shared/components/TextLayout.tsx
+++ src/shared/components/TextLayout.tsx
@@ -1,12 +1,14 @@
 interface TextLayoutProps{
     mainText: string
     subText?: string
+    mainTextSize?: 'title1' | 'title2'
+    center?: boolean
 }
 
-export default function TextLayout({ mainText, subText }: TextLayoutProps) {
+export default function TextLayout({ mainText, subText, mainTextSize = 'title1', center = false }: TextLayoutProps) {
   return (
-    <div className="flex flex-col gap-1">
-        <h1 className="text-title1 font-semibold whitespace-pre-line">{mainText}</h1>
+    <div className={`flex flex-col gap-1 ${center ? 'items-center text-center' : ''}`}>
+        <h1 className={`text-${mainTextSize} font-semibold whitespace-pre-line`}>{mainText}</h1>
         <p className="text-body text-gray-600">{subText}</p>
     </div>
   )
