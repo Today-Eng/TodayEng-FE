f54cf9c986366f42a1c8e1aeff352eb5df5e3164
kimseongyeon <seongyeon0810@naver.com>
2026-07-24 22:24:01 +0900

    chore: ESLint, Prettier 설정 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ function App() {
     <div className="flex items-center justify-center min-h-screen bg-gray-50">
       <h1 className="text-3xl text-title1 font-bold text-main-500">TodayEng</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
