ef2a2e88de0304335e4fb497b38b2842410e824a
kimseongyeon <seongyeon0810@naver.com>
2026-07-25 16:45:28 +0900

    refactor: BackHeaderLayout에 onBack prop 제거하고 뒤로가기 함수 추가

--- src/shared/components/BackHeaderLayout.tsx
+++ src/shared/components/BackHeaderLayout.tsx
@@ -1,14 +1,18 @@
+import { useNavigate } from 'react-router-dom'
 import chevronLeft from '../../assets/icons/chevron-left.svg'
 
 interface BackHeaderLayoutProps{
     title: string
-    onBack: () => void
 }
 
-export default function BackHeaderLayout({ title, onBack }: BackHeaderLayoutProps) {
+export default function BackHeaderLayout({ title }: BackHeaderLayoutProps) {
+  const navigate = useNavigate()
+  const goBack = () => {
+    navigate(-1)
+  }
   return (
     <div className='mt-[62px] flex gap-4 px-4 py-5'>
-        <button onClick={onBack}>
+        <button onClick={goBack}>
           <img src={chevronLeft} alt="" />
         </button>
         <div className='text-title2 font-semibold'>{title}</div>
