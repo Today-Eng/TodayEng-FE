e8a197de4f1fd07d2b2e5a416a79137471a41eb6
kimseongyeon <seongyeon0810@naver.com>
2026-07-25 16:46:50 +0900

    fix: Tailwind 동적 클래스 적용 안되는 문제 수정

--- src/shared/components/TextLayout.tsx
+++ src/shared/components/TextLayout.tsx
@@ -5,10 +5,15 @@ interface TextLayoutProps{
     center?: boolean
 }
 
+const sizeClass = {
+    title1: 'text-title1',
+    title2: 'text-title2',
+}
+
 export default function TextLayout({ mainText, subText, mainTextSize = 'title1', center = false }: TextLayoutProps) {
   return (
     <div className={`flex flex-col gap-1 ${center ? 'items-center text-center' : ''}`}>
-        <h1 className={`text-${mainTextSize} font-semibold whitespace-pre-line`}>{mainText}</h1>
+        <h1 className={`${sizeClass[mainTextSize]} font-semibold whitespace-pre-line`}>{mainText}</h1>
         <p className="text-body text-gray-600">{subText}</p>
     </div>
   )
